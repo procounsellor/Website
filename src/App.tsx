d94e55d2d255238aff75188d301f4d2f3d2dfa70
Ashutosh <ashutoshkkr60@gmail.com>
2025-08-30 20:38:58 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App(){
   return(
-    <div className="flex items-center justify-center">
-      <h1 className="text-sm font-extralight text-purple-900">This is procounsel website</h1>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-extralight text-purple-900">This is procounsel website</h1>
     </div>
   )
 }
