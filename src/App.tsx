fde4f7eaa870cb10b95f4a8d9804cc3bc542d285
UdayLabishetty <udaylabishetty2102@gmail.com>
2025-09-02 01:45:24 +0530

    book-appointment-section

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
+import { CounselorSection } from "./components/CounselorSection";
 import { Navbar } from "./components/Header";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
+
 
 
 export default function App(){
   return(
     <div>
-         <Navbar/>
-         <Card className="w-full max-w-xl">
-          <CardHeader>
-            <CardTitle>
-              anything
-            </CardTitle>
-           
-          </CardHeader>
-           <CardContent>
-              <p>my content</p>
-            </CardContent>
-
-            <CardFooter>
-              <p>card footer</p>
-            </CardFooter>
-         </Card>
+        <Navbar/>
+        <CounselorSection />        
     </div>
   )
 }
