8f89b18e6e605fd751e569a60bf150f52f4aba68
Ashutosh <ashutoshkkr60@gmail.com>
2025-09-07 12:04:55 +0530

    few comments

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,10 +1,52 @@
-export async function sendOtp(phone: string) {
-    console.log(phone)
-    // waiting for sendotp api because MGG doesn't allow calling from browser
+import {API_CONFIG} from './config'
+
+const baseUrl = API_CONFIG.baseUrl
+
+
+export async function  sendOtp(phone:string) {
+    try{
+        const response = await fetch(`${baseUrl}/api/auth/generateOtp?phoneNumber=${phone}`, {
+            method:'POST',
+            headers:{
+                Accept:'application/json'
+            }
+        })
+
+        if(!response.ok){
+            throw new Error(`HTTP ${response.status}: Failed to send otp`)
+        }
+
+        const data = await response.json()
+        console.log('Send otp response:', data)
+
+        return data
+
+    }catch(error){
+        console.error(error)
+        throw(error)
+    }
+    
 }
 
 export async function verifyOtp(phone: string, otp: string) {
-   console.log(phone, otp)
+    try {
+        const response = await fetch(`${baseUrl}/api/auth/verifyAndUserSignup?phoneNumber=${phone}&otp=${otp}`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
 
-//    same reason
+        if (!response.ok) {
+            throw new Error(`HTTP ${response.status}: OTP verification failed`)
+        }
+
+        const data = await response.json()
+        console.log('Verify OTP Response:', data)
+        return data
+
+    } catch (error) {
+        console.error('Verify OTP Error:', error)
+        throw error
+    }
 }
--- src/store/AuthStore.tsx
+++ src/store/AuthStore.tsx
@@ -24,10 +24,14 @@ export const useAuthStore = create<AuthState>()(
 
       sendOtp: async (phone: string) => {
         await sendOtp(phone);
+        // any number just include 8 consecutive zeros zero 10 digit total
       },
 
       verifyOtp: async (phone: string, otp: string) => {
         await verifyOtp(phone, otp);
+        // you will change this logic for now pass any otp 000000
+        // here if user doesn't exist then we will not mark isLoginToggle false for now you can remove it and work on next pages 
+        // and directly make it false from login card by importing the isLoginToggle from useAuthStore as well
         set({
           user: { id: phone },
           isAuthenticated: true,
