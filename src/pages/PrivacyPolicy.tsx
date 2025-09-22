79903f74cb4bdd985c2b51b2f79e1a613e9f10d6
Ashutosh <ashutoshkkr60@gmail.com>
2025-09-22 22:43:55 +0530

    changed error

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function PrivacyPolicyPage() {
   return (
     <div className="min-h-screen bg-gray-50">
--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function TermsPage() {
   return (
     <div className="min-h-screen bg-gray-50">
