ddcf07ebc559b5279cf07917fd771eb63dc53bd5
Ashutosh <ashutoshkkr60@gmail.com>
2025-10-07 22:49:47 +0530

    counselor dashboard

--- src/components/counselor-dashboard/index.ts
+++ src/components/counselor-dashboard/index.ts
@@ -1,4 +1,7 @@
 export { default as AppointmentPopup } from './AppointmentPopup';
 export { default as OutOfOfficeDrawer } from './OutOfOfficeDrawer';
 export { OutOfOfficeCard } from './OutOfOfficeCard';
+export { default as MyEarningsTab } from './MyEarningsTab';
+export { default as ClientsTab } from './ClientsTab';
+export { default as ReviewsTab } from './ReviewsTab';
 export * from './modals';
